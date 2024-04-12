1e8760efe6c51484482ec188ccb8ffe0a98f4116
[Prajwal Patil] <[prajwalpatil.2014@gmail.com]>
2024-04-12 18:58:17 +0100

    Using MVC for the new plant post. Multiple changes

--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -1,13 +1,59 @@
 const Plant = require('../models/plant');
+const PlantDetails = require('../models/plantDetails');
 
 exports.createPlant = async (req, res) => {
     try {
-        const plant = await Plant.create(req.body);
-        res.status(201).json(plant);
-    } catch (err) {
+        // Extract plant data from the request body
+        const { name, date_time_plant_seen ,description, createdby, latitude, longitude, height, spread, has_flowers, has_leaves, has_fruitsorseeds, flower_color } = req.body;
+    
+        const plantid = generateRandomID();
+        const plantdetailid = generateRandomID();
+
+        const createddate = new Date();
+        // Extract the filename of the uploaded image
+        const image = req.file.path;
+
+        // Create a new plant using the Plant model
+        const newPlant = await Plant.create({
+            plantid,
+            name,
+            description,
+            image, // Save the filename of the image
+            latitude,
+            longitude,
+            createdby,
+            createddate // You may need to parse this if it's coming as a string
+            // Add other fields as needed
+        });
+
+        const newPlantDetail = await PlantDetails.create({
+            plantdetailid,
+            height,
+            spread,
+            has_flowers,
+            has_leaves,
+            has_fruitsorseeds,
+            flower_color,
+            plantid,
+            date_time_plant_seen,
+        })
+
+        // Save the new plant to the database
+        await newPlant.save();
+        await newPlantDetail.save();
+
+        // Redirect to localhost:3000 after successful creation
+        res.redirect('http://localhost:3000/');
+        } catch (err) {
+        // Handle errors if any
         res.status(400).json({ message: err.message });
     }
 };
+
+function generateRandomID(){
+    const randomNumber = Math.floor(Math.random() * 1000);
+    return randomNumber.toString();
+}
 
 exports.getAllPlants = async (req, res) => {
     try {
--- server/models/plantDetails.js
+++ server/models/plantDetails.js
@@ -8,8 +8,9 @@ const plantDetailSchema = new mongoose.Schema({
     has_flowers: { type: Boolean },
     has_leaves: { type: Boolean },
     has_fruitsorseeds: { type: Boolean },
-    sunexposure: { type: String },
-    plantid: { type: Number, required: true }
+    flower_color: { type: String },
+    plantid: { type: Number, required: true },
+    date_time_plant_seen: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('PlantDetail', plantDetailSchema);
