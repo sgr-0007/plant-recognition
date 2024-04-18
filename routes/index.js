9151dad4c151f2a9fd79be98e2b8002913477f10
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2024-04-18 12:22:18 +0100

    plant details client

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,33 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/plantdetails', async (req, res) => {
+  const plantData =
+    {
+      plantdetailid: 2,
+      height: 50,
+      spread: 30,
+      has_flowers: false,
+      has_leaves: true,
+      has_fruitsorseeds: true,
+      flower_color: 'Blue',
+      plantid: 567,
+      date_time_plant_seen: new Date('2024-04-15T15:00:00'), // Example date
+    }
+    const plantid = 844;
+  try {
+      const response = await fetch(`http://localhost:5000/api/plant_details/${plantid}`);
+      if (response.ok) {
+          const plantDetails = await response.json();
+          res.render('plantdetails', {plantDetails});
+      } else {
+          throw new Error('Failed to fetch plant data');
+      }
+
+  } catch (err) {
+    console.error('Error while fetching data: ', err);
+    res.render('error', { message: 'Failed to fetch plant data' });
+  }
+});
+
 module.exports = router;
