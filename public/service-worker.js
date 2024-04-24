88b0911644549866092d5795267c42f29026e608
Sumit <akoliyasumit@hotmail.com>
2024-04-24 17:04:26 +0100

    Caching homepage to load when offline

---
+++ public/service-worker.js
@@ -0,0 +1,65 @@
+// public/service-worker.js
+
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    '/',
+    '/stylesheets/style.css',
+    // '/views/index.ejs',
+    // '/views/navbar.css',
+    // '/javascripts/app.js',
+    // '/javascripts/db.js',
+    // '/images/logo.png',
+    '/index.ejs',
+    '/navbar.ejs',
+    '/plantdetails.ejs'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    var responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then((cache) => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    var cacheAllowlist = ['v1'];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheAllowlist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
