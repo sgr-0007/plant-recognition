5c3ecad80aae04f2cff8f4e5ed8f9afaf6a6cdbf
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2024-05-06 16:29:06 +0100

    chat-offline

--- public/util/chat.js
+++ public/util/chat.js
@@ -1,26 +1,49 @@
-let name = localStorage.getItem("username") || prompt('What is your name?');
-localStorage.setItem("username", name);
-
+let name = null;
 let roomNo = null;
 let socket = io();
 let plantid = "";
+var storedUsername = localStorage.getItem("username");
+if (storedUsername) {
+    name = storedUsername;
+} else {
+    name = prompt('What is your name?');
+    localStorage.setItem("username", name);
+}
 
 function isOnline() {
     return navigator.onLine;
 }
 
 function init() {
+
     const urlParams = new URLSearchParams(window.location.search);
     plantid = urlParams.get('plantid');
     roomNo = plantid;
     connectToRoom();
-
-    socket.on('joined', handleUserJoinedRoom);
-    socket.on('chat', handleChatReceived);
+    // called when someone joins the room. If it is someone else it notifies the joining of the room
+    socket.on('joined', function (room, userId) {
+        if (userId === name) {
+            // it enters the chat
+            hideLoginInterface(room, userId);
+        } else {
+            // notifies that someone has joined the room
+            writeOnHistory('<b>' + userId + '</b>' + ' joined room ' + room);
+        }
+    });
+    // called when a message is received
+    socket.on('chat', function (room, userId, chatText) {
+        let who = userId
+        if (userId === name) who = 'Me';
+        writeOnHistory('<b>' + who + ':</b> ' + chatText);
+    });
 
     getChat();
 }
 
+/**
+ * It gets the text to send from the interface
+ * and sends the message via  socket
+ */
 function sendChatText() {
     if (!isOnline()) {
         const discussionFormData = {
@@ -28,52 +51,63 @@ function sendChatText() {
             commentedby: name,
             comment: document.getElementById("chat_input").value
         };
+        console.log("Collected suggestion data: ", discussionFormData);
+        openPlantsIDB().then((db) => {
+            const plantId = parseInt(plantid);
+            console.log("PLANT ID: ", plantid);
+            getPlantById(db, plantId)
+                .then((plant) => {
+                    if (plant) {
+                        console.log("PLANT FOUND");
+                        const commentID = Math.floor(Math.random() * 10000);
 
-        saveChatOffline(discussionFormData.comment);
-    } else {
+                        const newComment = {
+                            commentid: commentID,
+                            commentedby: discussionFormData.commentedby,
+                            comment: discussionFormData.comment
+                        };
+
+                        plant.comments.push(newComment);
+                        openPlantsIDB().then((db) => {
+                            const transaction = db.transaction(["plants"], "readwrite");
+                            const plantStore = transaction.objectStore("plants");
+                            const updateRequest = plantStore.put(plant);
+                            let chatText = document.getElementById('chat_input').value;
+                            socket.emit('chat', roomNo, newComment.commentedby, newComment.comment);
+
+                            updateRequest.onsuccess = () => {
+                                console.log("Plant data updated successfully.");
+                                alert("You are offline. Your comment will be saved and synced when you are back online.");
+                                
+                            };
+
+                            updateRequest.onerror = (event) => {
+                                console.error("Error updating plant data:", event.target.error);
+                            };
+                        })
+                        // Update the plant data in IndexedDB
+                    } else {
+                        console.log("PLANT NOT FOUND");
+                    }
+                })
+        })
+    }
+    else {
         let chatText = document.getElementById('chat_input').value;
         socket.emit('chat', roomNo, name, chatText);
-        saveChat(chatText, Math.floor(Math.random() * 100000) + 1);
+        saveChat(chatText,Math.floor(Math.random() * 100000) + 1);
     }
+
 }
 
-function saveChatOffline(commentText) {
-    openPlantsIDB().then((db) => {
-        const plantId = parseInt(plantid);
-        getPlantById(db, plantId)
-            .then((plant) => {
-                if (plant) {
-                    const commentID = Math.floor(Math.random() * 10000);
-                    const newComment = {
-                        commentid: commentID,
-                        commentedby: name,
-                        comment: commentText
-                    };
-                    plant.comments.push(newComment);
-
-                    const transaction = db.transaction(["plants"], "readwrite");
-                    const plantStore = transaction.objectStore("plants");
-                    const updateRequest = plantStore.put(plant);
-
-                    updateRequest.onsuccess = () => {
-                        alert("You are offline. Your comment will be synced when you are back online.");
-                    };
-
-                    updateRequest.onerror = (event) => {
-                        console.error("Error updating plant data:", event.target.error);
-                    };
-                }
-            });
-    });
-}
-
-function saveChat(chatText, commentId) {
+function saveChat(chatTextInput, commentId) {
+    let chatText = chatTextInput;
     let chat = {
-        updateCommentId: commentId,
+        updateCommentId : commentId,
         comment: chatText,
         commentedby: name
-    };
-
+    }
+    console.log('Chat:', chat);
     fetch(`http://localhost:3000/api/plantdetails/${plantid}/comments`, {
         method: 'POST',
         headers: {
@@ -81,13 +115,13 @@ function saveChat(chatText, commentId) {
         },
         body: JSON.stringify(chat),
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('ChatSuccess:', data);
-    })
-    .catch((error) => {
-        console.error('ChatError:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('ChatSuccess:', data);
+        })
+        .catch((error) => {
+            console.error('ChatError:', error);
+        });
 }
 
 function getChat() {
@@ -97,19 +131,28 @@ function getChat() {
             'Content-Type': 'application/json',
         }
     })
-    .then(response => response.json())
-    .then(getChatAndUpdateHistory)
-    .catch((error) => {
-        console.error('getChatError:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            console.log('getChatSuccess:', data);
+            data.comments.forEach(comment => {
+                let who = comment.commentedby
+                if (comment.commentedby === name) who = 'Me';
+                writeOnHistory('<b>' + who + ':</b> ' + comment.comment);
+            });
+        })
+        .catch((error) => {
+            console.error('getChatError:', error);
+        });
 }
 
 function getIdbChatAndPushIntoNetworkDb() {
     openPlantsIDB().then((db) => {
         const plantId = parseInt(plantid);
+        console.log("PLANT ID: ", plantid);
         getPlantById(db, plantId)
             .then((plant) => {
                 if (plant) {
+                    console.log("PLANT FOUND");
                     const saveChatPromises = [];
                     plant.comments.forEach(comment => {
                         saveChatPromises.push(saveChat(comment.comment, comment.commentid));
@@ -117,8 +160,10 @@ function getIdbChatAndPushIntoNetworkDb() {
 
                     Promise.all(saveChatPromises)
                         .then(() => {
+                            let history = document.getElementById('history');
+                            history.innerHTML = '';
                             console.log("All comments saved successfully");
-                            getChatAndUpdateHistory(plant);
+                            getChat(); 
                         })
                         .catch(error => {
                             console.error("Error saving comments:", error);
@@ -130,30 +175,23 @@ function getIdbChatAndPushIntoNetworkDb() {
     });
 }
 
+
+// call getsychchat when the network is online using event listener
 window.addEventListener('online', () => {
     alert("You are back online. Your comments will be synced now.");
     getIdbChatAndPushIntoNetworkDb();
 });
+ 
 
 function connectToRoom() {
+
     if (!name) name = 'Unknown-' + Math.random();
     socket.emit('create or join', roomNo, name);
 }
-
-function handleUserJoinedRoom(room, userId) {
-    if (userId === name) {
-        hideLoginInterface(room, userId);
-    } else {
-        writeOnHistory('<b>' + userId + '</b>' + ' joined room ' + room);
-    }
-}
-
-function handleChatReceived(room, userId, chatText) {
-    let who = userId;
-    if (userId === name) who = 'Me';
-    writeOnHistory('<b>' + who + ':</b> ' + chatText);
-}
-
+/**
+ * it appends the given html text to the history div
+ * @param text: teh text to append
+ */
 function writeOnHistory(text) {
     let history = document.getElementById('history');
     let paragraph = document.createElement('p');
@@ -162,7 +200,15 @@ function writeOnHistory(text) {
     document.getElementById('chat_input').value = '';
 }
 
+/**
+ * it hides the initial form and shows the chat
+ * @param room the selected room
+ * @param userId the user name
+ */
 function hideLoginInterface(room, userId) {
+
     document.getElementById('who_you_are').innerHTML = userId;
     document.getElementById('in_room').innerHTML = ' ' + room;
 }
+
+
