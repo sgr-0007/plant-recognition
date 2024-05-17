b3343bd19d39acc185e9ef1f3572004fcffd09e6
Sagar Honnenahalli somashekhar <shonnenahallisomashekhar1@sheffield.ac.uk>
2024-05-17 01:10:58 +0100

    removed old server

--- old_server/routes/plantDetailsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {getPlantDetailsByPlantId} = require("../controllers/plantDetailsController");
-const router = express.Router();
-
-
-router.get('/:plantid', getPlantDetailsByPlantId);
-
-module.exports = router;
